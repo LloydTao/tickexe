1108b6a03966e25dcd5d94631c7b333911672f7d
LloydTao <LL23@live.co.uk>
2021-10-15 22:10:19 +0100

    Rename listing creator to user

--- backend/listings/models/listing.py
+++ backend/listings/models/listing.py
@@ -5,9 +5,7 @@ from accounts.models import User
 
 
 class Listing(models.Model):
-    creator = models.ForeignKey(
-        User, models.CASCADE, related_name="listings"
-    )
+    user = models.ForeignKey(User, models.CASCADE, related_name="listings")
     price = models.DecimalField(max_digits=1000000, decimal_places=2)
     venue = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
